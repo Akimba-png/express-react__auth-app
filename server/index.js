531d822af3ca1c542c408f01aa5063b49084f60f
Oleg Akimov <akimba@gmail.com>
2024-01-15 17:10:06 +0300

    feat: initialize server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import 'dotenv/config';
+
+const port = process.env.PORT
+
+const app = express();
+
+app.listen(port, () => console.log(`server is listening on port ${port}`));
