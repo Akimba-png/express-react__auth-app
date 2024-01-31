5c3990520a7412bfd9d732300fad0ae93ec63125
Oleg Akimov <akimba@gmail.com>
2024-01-31 22:17:28 +0300

    fix: Cors error fixed

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import 'dotenv/config';
 import { authRouter } from './routers/auth-router.js';
 import { errorMiddleware } from './middlewares/error-middleware.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use('/auth', authRouter);
 app.use(errorMiddleware);
 
